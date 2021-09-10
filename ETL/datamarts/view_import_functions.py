3b400b7e56e2157fc092eee36904c847714d4249
frelyf <fwlyford@gmail.com>
2021-09-10 13:32:14 +0200

    Machine learning updated

--- ETL/datamarts/view_import_functions.py
+++ ETL/datamarts/view_import_functions.py
@@ -1,6 +1,5 @@
 import psycopg2
 import pandas as pd
-import numpy as np
 
 def get_connection():
     connection = psycopg2.connect(
@@ -12,9 +11,17 @@ def get_connection():
             )
     return connection
 
+def get_df_simple(aggregator):
+    engine = get_connection()
+    query = f"""select * from {aggregator}_values_per_date"""
+    df = pd.read_sql_query(query, con = engine)
+    return df
+
+
 def get_df_with_lags():
     engine = get_connection()
     
+    #Queries
     month_lag_query = """select * from avg_values_month_lag"""
     bi_month_lag_query = """select * from avg_values_bi_month_lag"""
     qrt_year_lag_query = """select * from avg_values_qrt_year_lag"""
@@ -30,25 +37,31 @@ def get_df_with_lags():
     df_half_year_lag = pd.read_sql_query(half_year_lag_query, con = engine)
     df_yearlag = pd.read_sql_query(year_lag_query, con = engine)
     
-    # Remove dateid_serial from lags
+    # Remove dateid_serial from lags and rename to dateid_serial for merging
     del df_month_lag['dateid_serial']
     del df_bi_month_lag['dateid_serial']
     del df_qrt_year_lag['dateid_serial']
     del df_half_year_lag['dateid_serial']
     del df_yearlag['dateid_serial']
     
-    # Merge lags
-    df = df.merge(df_month_lag, how = 'left', left_on='dateid_serial', right_on='month_lag')
-    df = df.merge(df_bi_month_lag, how = 'left', left_on='dateid_serial', right_on='bi_month_lag')
-    df = df.merge(df_qrt_year_lag, how = 'left', left_on='dateid_serial', right_on='qrt_year_lag')
-    df = df.merge(df_half_year_lag, how = 'left', left_on='dateid_serial', right_on='half_year_lag')
-    df = df.merge(df_yearlag, how = 'left', left_on='dateid_serial', right_on='year_lag')
+    df_month_lag['dateid_serial'] = df_month_lag['month_lag']
+    df_bi_month_lag['dateid_serial'] = df_bi_month_lag['bi_month_lag']
+    df_qrt_year_lag['dateid_serial'] = df_qrt_year_lag['qrt_year_lag']
+    df_half_year_lag['dateid_serial'] = df_half_year_lag['half_year_lag']
+    df_yearlag['dateid_serial'] = df_yearlag['year_lag']
     
-    del df['month_lag']
-    del df['bi_month_lag']
-    del df['qrt_year_lag']
-    del df['half_year_lag']
-    del df['year_lag']
+    del df_month_lag['month_lag']
+    del df_bi_month_lag['bi_month_lag']
+    del df_qrt_year_lag['qrt_year_lag']
+    del df_half_year_lag['half_year_lag']
+    del df_yearlag['year_lag']
+    
+    # Merge lags
+    df = df.merge(df_month_lag, how = 'left', on = 'dateid_serial')
+    df = df.merge(df_bi_month_lag, how = 'left', on = 'dateid_serial')
+    df = df.merge(df_qrt_year_lag, how = 'left', on = 'dateid_serial')
+    df = df.merge(df_half_year_lag, how = 'left', on = 'dateid_serial')
+    df = df.merge(df_yearlag, how = 'left', on = 'dateid_serial')
     
     #Return df
     return df
@@ -57,6 +70,7 @@ def get_df_with_lags():
 def get_df_with_lags_per_area(coordinate):    
     engine = get_connection()
 
+    # Queries
     month_lag_query = f"""select * from avg_values_month_lag_{coordinate}"""
     bi_month_lag_query = f"""select * from avg_values_bi_month_lag_{coordinate}"""
     qrt_year_lag_query = f"""select * from avg_values_qrt_year_lag_{coordinate}"""
@@ -72,7 +86,7 @@ def get_df_with_lags_per_area(coordinate):
     df_half_year_lag = pd.read_sql_query(half_year_lag_query, con = engine)
     df_yearlag = pd.read_sql_query(year_lag_query, con = engine)
     
-    # Remove dateid_serial from lags
+    # Remove dateid_serial from lags and rename to dateid_serial for merging
     del df_month_lag['dateid_serial']
     del df_bi_month_lag['dateid_serial']
     del df_qrt_year_lag['dateid_serial']
--- analysis/machine_learning/first_model.py
+++ analysis/machine_learning/first_model.py
@@ -1,15 +1,14 @@
-import psycopg2
+# Import libraries
 import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 from sklearn.tree import DecisionTreeRegressor, plot_tree
-from sklearn.model_selection import train_test_split
+from sklearn.impute import KNNImputer
 import datetime as dt
 import sklearn
 import xgboost as xgb
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.linear_model import LinearRegression
@@ -20,71 +19,58 @@ from tensorflow.keras.layers import Dense, Input, Concatenate, Dropout, BatchNor
 from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras.losses import SparseCategoricalCrossentropy
 from tensorflow.keras import regularizers
-from keras_tuner import RandomSearch, Hyperband
-from keras import backend as K
-from tensorflow.keras.optimizers import Adam
 import matplotlib.pyplot as plt
 
-def get_connection():
-    connection = psycopg2.connect(
-                host="trafikkluft.postgres.database.azure.com",                
-                port="5432",
-                user="postgres@trafikkluft",                
-                password="Awesome1337",                
-                database="postgres",            
-            )
-    return connection
+from etl.datamarts.view_import_functions import get_df_simple, get_df_with_lags, get_df_with_lags_per_area
 
-engine = get_connection()
+df_avg = get_df_with_lags()
+df_avg = df_avg.sort_values(by='dateid_serial', ignore_index = True)
+df_avg = df_avg.iloc[364:].reset_index()
 
-max_query = """
-    select * from max_values_per_date
-"""
 
-min_query = """
-    select * from min_values_per_date
-"""
+X_cols = [
+    'traffic_volume', 'precipitation', 'wind_speed',
+    'air_temperature', 'pm2_5','pm10', 'nox', 'no',
+    'traffic_volume_lag_1', 'precipitation_lag_1', 'wind_speed_lag_1',
+    'air_temperature_lag_1', 'pm2_5_lag_1', 'pm10_lag_1', 'nox_lag_1',
+    'no2_lag_1', 'no_lag_1', 'traffic_volume_lag_2', 'precipitation_lag_2',
+    'wind_speed_lag_2', 'air_temperature_lag_2', 'pm2_5_lag_2',
+    'pm10_lag_2', 'nox_lag_2', 'no2_lag_2', 'no_lag_2',
+    'traffic_volume_lag_3', 'precipitation_lag_3', 'wind_speed_lag_3',
+    'air_temperature_lag_3', 'pm2_5_lag_3', 'pm10_lag_3', 'nox_lag_3',
+    'no2_lag_3', 'no_lag_3', 'traffic_volume_lag_6', 'precipitation_lag_6',
+    'wind_speed_lag_6', 'air_temperature_lag_6', 'pm2_5_lag_6',
+    'pm10_lag_6', 'nox_lag_6', 'no2_lag_6', 'no_lag_6',
+    'traffic_volume_lag_12', 'precipitation_lag_12', 'wind_speed_lag_12',
+    'air_temperature_lag_12', 'pm2_5_lag_12', 'pm10_lag_12', 'nox_lag_12',
+    'no2_lag_12', 'no_lag_12'
+]
 
-avg_query = """
-    select * from avg_values_per_date
-"""
+y_col = ['no2']
 
-df_max = pd.read_sql_query(max_query, con = engine)
-df_min = pd.read_sql_query(min_query, con = engine)
-df_avg = pd.read_sql_query(avg_query, con = engine)
+# Imputing values
+imputer_X = KNNImputer(n_neighbors = 5)
+imputer_X.fit(df_avg[X_cols])
+df_avg[X_cols] = imputer_X.transform(df_avg[X_cols])
 
-# Log scaling for df_avg
-# df_avg['precipitation'] = np.log(df_avg['precipitation'])
+imputer_y = KNNImputer(n_neighbors = 5)
+imputer_y.fit(df_avg[y_col])
+df_avg[y_col] = imputer_y.transform(df_avg[y_col])
 
 # StandardScaling for df_avg
 s_scaler = StandardScaler()
-s_scaler.fit(df_avg[['traffic_volume','wind_speed','air_temperature', 'precipitation']])
-
-df_avg[['traffic_volume','wind_speed','air_temperature', 'precipitation']] = s_scaler.transform(df_avg[['traffic_volume','wind_speed','air_temperature', 'precipitation']])
-df_avg.hist(bins = 50)
+s_scaler.fit(df_avg[X_cols])
+df_avg[X_cols] = s_scaler.transform(df_avg[X_cols])
 
 # Splitting into train and test, X and y
 df_avg_test = df_avg.iloc[:365]
 df_avg_train = df_avg.iloc[365:]
 
-X_train = np.c_[df_avg_train[['traffic_volume', 'precipitation','wind_speed','air_temperature']]]
-X_test = np.c_[df_avg_test[['traffic_volume', 'precipitation','wind_speed','air_temperature']]]
+X_train = np.c_[df_avg_train[X_cols]]
+X_test = np.c_[df_avg_test[X_cols]]
 
-pm2_5_train = np.c_[df_avg_train['pm2_5']]
-pm2_5_test = np.c_[df_avg_test['pm2_5']]
-
-pm10_train = np.c_[df_avg_train['pm10']]
-pm10_test = np.c_[df_avg_test['pm10']]
-
-nox_train = np.c_[df_avg_train['nox']]
-nox_test = np.c_[df_avg_test['nox']]
-
-no_train = np.c_[df_avg_train['no']]
-no_test = np.c_[df_avg_test['no']]
-
-no2_train = np.c_[df_avg_train['no2']]
-no2_test = np.c_[df_avg_test['no2']]
-
+y_train = np.c_[df_avg_train[y_col]]
+y_test = np.c_[df_avg_test[y_col]]
 # Multivariate
 
 def multivariate_regressor(X_train, y_train, X_test, y_test):
@@ -114,7 +100,7 @@ def multivariate_regressor(X_train, y_train, X_test, y_test):
     {corr_matrix}
           ''')
           
-multivariate_regressor(X_train, nox_train, X_test, nox_test)
+multivariate_regressor(X_train, y_train, X_test, y_test)
 
 # K Nearest Neighbor
 def kneighborsregressor(X_train, y_train, X_test, y_test):    
@@ -146,7 +132,7 @@ def kneighborsregressor(X_train, y_train, X_test, y_test):
     {corr_matrix}
           ''')
           
-kneighborsregressor(X_train, nox_train, X_test, nox_test)
+kneighborsregressor(X_train, y_train, X_test, y_test)
 
 # XGBoost
 
@@ -175,42 +161,36 @@ def xgb_regressor(X_train, y_train, X_test, y_test):
     Train MAE = {mae_train}
             ''')
 
-xgb_regressor(X_train, nox_train, X_test, nox_test)
+xgb_regressor(X_train, y_train, X_test, y_test)
 
 
 # DNN
 
-input_layer = Input(shape = (4,))
+input_layer = Input(shape = (53,))
 second_hidden_layer = Dense(32, activation = 'relu')(input_layer)
 first_dropout_layer = Dropout(0.2)(second_hidden_layer)
-third_hidden_layer = Dense(16, activation = 'relu')(first_dropout_layer)
-first_regularization_layer = Dense(8, kernel_regularizer = regularizers.l2(0.01))(third_hidden_layer)
+third_hidden_layer = Dense(20, activation = 'relu')(first_dropout_layer)
+first_regularization_layer = Dense(10, kernel_regularizer = regularizers.l2(0.01))(third_hidden_layer)
 second_dropout_layer = Dropout(0.1)(first_regularization_layer)
-fourth_hidden_layer = Dense(4, activation = 'relu')(second_dropout_layer)
+fourth_hidden_layer = Dense(5, activation = 'relu')(second_dropout_layer)
 output_layer = Dense(1)(fourth_hidden_layer)
 
 model = Model(inputs = input_layer, outputs = output_layer)
 model.compile(optimizer = 'adam', loss = 'mse', metrics = ['mae'])
 history = model.fit(
     X_train, 
-    nox_train, 
-    batch_size=32, 
-    epochs=750,
-    validation_data = (X_test, nox_test))
+    y_train, 
+    batch_size=256, 
+    epochs=3000,
+    validation_data = (X_test, y_test))
 
 
-def plot_mse():
+def plot():
     plt.plot(np.sqrt(history.history['loss']))
     plt.plot(np.sqrt(history.history['val_loss']))
-    plt.legend(['loss','val_loss'])
-    plt.show()
-
-plot_mse()
-
-def plot_mae():
     plt.plot(history.history['mae'], label = 'mae')
     plt.plot(history.history['val_mae'], label = 'val_mae')
-    plt.legend(['mae','val_mae'])
+    plt.legend(['mae','val_mae', 'loss','val_loss'])
     plt.show()
 
-plot_mae()
+plot()
