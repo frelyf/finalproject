0ae78f9b83770055ff93b4c39ce288a18acd3fcc
frelyf <fwlyford@gmail.com>
2021-09-15 16:06:55 +0200

    Ferdig analyse på vær og trafikk

--- analysis/machine_learning/ml_analysis.py
+++ analysis/machine_learning/ml_analysis.py
@@ -1,7 +1,10 @@
 from analysis.machine_learning.ml_models import xgb_regressor
 from analysis.data_prep import get_ml_test_train
 from etl.datamarts.view_import_functions import get_df_with_lags
+from sklearn.preprocessing import StandardScaler
 import json
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
 
@@ -64,16 +67,88 @@ for basis, feature_names in zip(basis_list, feature_names_list):
         feature_importance_named = {}
         for feature_name, importance in zip(feature_names, feature_importance.values()):
             feature_importance_named[feature_name] = importance
-        
         feature_importance_dict[value] = feature_importance_named
     
     y_pred_basis_dict[basis] = y_pred_dict
     stats_basis_dict[basis] = stats_dict
     feature_importance_basis_dict[basis] = feature_importance_dict
+
+y_true_dict = {}    
+for value in values:
+    X_train, X_test, y_train, y_test = get_ml_test_train(value = value, basis = basis, latitude_category = False, longitude_category = False)
+    y_true_dict[value] = y_test.flatten().tolist()
+y_pred_basis_dict['true_values'] = y_true_dict
+    
 json_dict = {
     'y_pred': y_pred_basis_dict,
     'stats': stats_basis_dict,
     'feature_importance': feature_importance_basis_dict}
 
-with open(r'files\analysis.json', 'w') as outfile:
-    json.dump(json_dict, outfile)
+# with open(r'files\analysis.json', 'w') as outfile:
+#     json.dump(json_dict, outfile)
+
+
+df_pred_all = pd.DataFrame(y_pred_basis_dict[None])
+df_pred_wt = pd.DataFrame(y_pred_basis_dict['weather and traffic']) 
+df_pred_w= pd.DataFrame(y_pred_basis_dict['weather'])
+df_true= pd.DataFrame(y_pred_basis_dict['true_values'])
+df_pred_t= pd.DataFrame(y_pred_basis_dict['traffic'])
+
+df_list = [df_pred_all, df_pred_wt, df_pred_w, df_pred_t]
+
+
+
+def plot_all():
+    for value in values:
+        for df in df_list:
+            plt.plot(df[value])
+        
+    plt.show()
+
+def plot_true_and_pred():
+    basis_list = ["All features", 'weather and traffic', 'weather', 'true','traffic']
+    for basis, df in zip(basis_list,df_list):
+        plt.cla()
+        plt.scatter(df_pred_true['nox'], df['nox'])
+        plt.ylabel('True')
+        plt.xlabel(basis)
+        
+        x = [i for i in range(int(df_pred_true.max().max()))]
+        y = [i for i in range(int(df_pred_true.max().max()))]
+    
+        plt.plot(x,y, '--', color = '#1C2833')
+        plt.savefig(f'files\plots\{basis}')
+        
+
+df_scaling = get_df_with_lags()[['pm2_5', 'pm10', 'no', 'no2', 'nox']]
+pred_scaler = StandardScaler()
+pred_scaler.fit(df_scaling)
+
+df_pred_all = pd.DataFrame(pred_scaler.transform(df_pred_all), columns = ['pm2_5', 'pm10', 'no', 'no2', 'nox'])
+df_pred_wt = pd.DataFrame(pred_scaler.transform(df_pred_wt), columns = ['pm2_5', 'pm10', 'no', 'no2', 'nox'])
+df_pred_w = pd.DataFrame(pred_scaler.transform(df_pred_w), columns = ['pm2_5', 'pm10', 'no', 'no2', 'nox'])
+df_true = pd.DataFrame(pred_scaler.transform(df_true), columns = ['pm2_5', 'pm10', 'no', 'no2', 'nox'])
+df_pred_t = pd.DataFrame(pred_scaler.transform(df_pred_t), columns = ['pm2_5', 'pm10', 'no', 'no2', 'nox'])
+
+df_pred_all_error = df_true.subtract(df_pred_all).abs().sum(axis = 1).to_frame(name = 'all_features')
+df_pred_wt_error = df_true.subtract(df_pred_wt).abs().sum(axis = 1).to_frame(name = 'weather_traffic')
+df_pred_w_error = df_true.subtract(df_pred_w).abs().sum(axis = 1).to_frame(name = 'weather')
+df_pred_t_error = df_true.subtract(df_pred_t).abs().sum(axis = 1).to_frame(name = 'traffic')
+
+errors = pd.concat([df_pred_all_error, df_pred_wt_error, df_pred_w_error, df_pred_t_error], axis = 1)
+errors.plot()
+
+errors_smooth = errors.rolling(9).mean()
+errors_smooth = errors_smooth.iloc[::9, :].reset_index(drop = True)
+
+days = pd.date_range('2021-01-01', periods = 28, freq = '10d').to_frame(name = 'date').reset_index().drop(columns = ['index'])
+errors_smooth = pd.concat([errors_smooth, days], axis = 1).set_index('date', drop = True)
+errors_smooth.plot()
+
+m_min = errors.eq(errors.min(axis=1), 0)
+min_error = m_min.dot(errors.columns + ',').str.rstrip(',').value_counts()
+min_error = min_error.apply(lambda x: 100 * x / float(min_error.sum()))
+
+m_max = errors.eq(errors.max(axis=1), 0)
+max_error = m_max.dot(errors.columns + ',').str.rstrip(',').value_counts()
+max_error = max_error.apply(lambda x: 100 * x / float(max_error.sum()))
